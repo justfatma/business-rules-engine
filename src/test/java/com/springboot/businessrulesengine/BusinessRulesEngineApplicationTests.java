93c0a6abacba5cc6c00f441fe8022ae157afbd42
unknown <tasatan@gmail.com>
2022-01-01 20:24:51 +0300

    web service application created

---
+++ src/test/java/com/springboot/businessrulesengine/BusinessRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.businessrulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessRulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
