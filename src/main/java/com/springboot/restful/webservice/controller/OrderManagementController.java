22b8ba81a850852864777b6a442089d72ced6005
unknown <tasatan@gmail.com>
2022-01-04 13:42:13 +0300

    Request methods related to the product were added

--- src/main/java/com/springboot/restful/webservice/controller/OrderManagementController.java
+++ src/main/java/com/springboot/restful/webservice/controller/OrderManagementController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.springboot.restful.webservice.model.MemberOrder;
+import com.springboot.restful.webservice.model.Product;
 import com.springboot.restful.webservice.serviceimpl.MemberOrderServiceImpl;
+import com.springboot.restful.webservice.serviceimpl.ProductServiceImpl;
 
 @RestController
 public class OrderManagementController {
 
 	@Autowired
 	private MemberOrderServiceImpl memberOrderServiceImpl;
+	
+	@Autowired
+	private ProductServiceImpl productServiceImpl;
 	
 	@GetMapping("/orders")
     public List<MemberOrder> getOrders() {	
@@ -46,4 +50,23 @@ public class OrderManagementController {
     	Long addedOrderId= memberOrderServiceImpl.addMemberOrder(memberOrder);
     	return "Order created successfully. Order Id: " + addedOrderId;
     }
+    
+    @GetMapping("/products")
+    public List<Product> getProducts() {	
+    	List<Product> productList= productServiceImpl.getProductList();
+    	if (productList!= null && productList.size()>0) {
+    		return productList;
+    	}else {
+    		throw new ResponseStatusException(HttpStatus.NOT_FOUND) ;
+    	}
+    }
+	
+	@GetMapping("/products/{productId}")
+	public Product getProductById(@PathVariable Long productId) {
+				
+		return Optional.ofNullable(productServiceImpl.getProductById(productId))
+    			.get()
+    			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+    
 }
